2defd65fa9d907b94b4f41a4b7f6558811cb0308
Bruno Del Canto <delcantobruno5@gmail.com>
2026-02-03 19:09:49 -0300

    login and register implementation

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from 'express';
+import User from '../../models/user.js'
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const createToken = (id: string) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'secret_key', {
+        expiresIn: '8h'
+    });
+};
+
+const registerUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: 'User already exists' })
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role: 'seller'
+        });
+
+        const token = createToken(user._id.toString());
+
+        res.status(201).json({
+            message: 'User registered successfully',
+            token,
+            user: { id: user._id, name: user.name, email: user.email, role: user.role}
+        })
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Password is incorrect' });
+        }
+
+        const token = createToken(user._id.toString());
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            user: { id: user._id, name: user.name, email: user.email, role: user.role}
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export { registerUser, loginUser };
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,37 @@
-import {Schema, Document} from 'mongoose';
+import {Schema, Document, model} from 'mongoose';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export interface User extends Document {
     name: string;
     email: string;
     password: string;
-    role: 'admin' | 'developer';
+    role: 'admin' | 'seller' | 'developer';
     isActive: boolean;
 }
 
-const userSchema = new Schema<User>(
-    {
-        name: { type: String, required: true, trim: true },
-        email: { type: String, required: true, unique: true, trim: true },
-        password: { type: String, required: true },
-        role: { type: String, enum: ['admin', 'developer'], default: 'developer' },
-        isActive: { type: Boolean, default: true },
-    },
-    { 
-        timestamps: true 
+const UserSchema = new Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true, lowercase: true},
+    password: { type: String, required: true},
+    role: {
+        type: String,
+        enum: ['admin', 'seller', 'developer'],
+        default: 'admin'
     }
-)
+}, { timestamps: true });
 
-const User = mongoose.model<User>('User', userSchema);
+UserSchema.pre('save', async function(){
+    if (!this.isModified('password')) return;
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error: any) {
+        throw error;
+    }
+})
+
+const User = mongoose.model<User>('User', UserSchema);
 
 export default User;
